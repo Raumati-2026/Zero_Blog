eddf4c1bfd7e2b0a8372df836b170f57c9c8a0af
Jay Coupe <jay.huston17@gmail.com>
2026-03-26 16:14:37 +1300

    Completed migrations post

--- server/db/migrations/20260326021031_posts.js
+++ server/db/migrations/20260326021031_posts.js
@@ -2,14 +2,21 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+  return knex.schema.createTable('blog_entry', (table) => {
+    table.increments('id').primary()
+    table.string('title')
+    table.text('entry') // better than varchar for blog content
+    table.date('date')
+    table.integer('author_id').references('id').inTable('users')
+    table.string('topic')
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+  return knex.schema.dropTable('blog_entry')
+}
