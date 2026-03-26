0d597b9eb181d9e5f6389cc0bf36126c66f96c40
phoebe <phoebe@ThinkPad-P50S.localdomain>
2026-03-26 15:40:01 +1300

    add the users m

--- server/db/migrations/20260326021909_users.js
+++ server/db/migrations/20260326021909_users.js
@@ -2,14 +2,17 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+export const up = function (knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id').primary()
+    table.string('name')
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+export const down = function (knex) {
+  return knex.schema.dropTable('users')
+}
