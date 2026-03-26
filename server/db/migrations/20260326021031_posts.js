b25a3619af608cd3c27b82e5b6f3b399621540c5
Jay Coupe <jay.huston17@gmail.com>
2026-03-26 16:24:11 +1300

    Fixed the post migrations

--- server/db/migrations/20260326021031_posts.js
+++ server/db/migrations/20260326021031_posts.js
@@ -2,7 +2,7 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {
+export const up = function (knex) {
   return knex.schema.createTable('blog_entry', (table) => {
     table.increments('id').primary()
     table.string('title')
@@ -17,6 +17,6 @@ exports.up = function (knex) {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {
+export const down = function (knex) {
   return knex.schema.dropTable('blog_entry')
 }
