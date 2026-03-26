5281d6f4d34d2b9be363950f67d820cf8ec7aa88
Jullian Bublitz <jullianbublitz.garratt@gmail.com>
2026-03-26 15:43:48 +1300

    Added post component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from 'react-router'
 import Nav from './Nav.tsx'
 import Footer from './Footer.tsx'
+import { Post } from './Post.tsx'
 
 function App() {
   return (
     <>
-      <Nav/>
-      <Outlet/>
-      <Footer/>
+      <Nav />
+      <Outlet />
+      <Post></Post>
+      <Footer />
     </>
   )
 }
