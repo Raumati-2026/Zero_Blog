6368677f66b54134301da604820e2a47fd68020a
Jullian Bublitz <jullianbublitz.garratt@gmail.com>
2026-03-26 15:59:23 +1300

    route and post page

---
+++ client/components/PostPage.tsx
@@ -0,0 +1,35 @@
+function PostPage() {
+  const post_data = {
+    id: 1,
+    title: 'Example Post',
+    entry: 'Mucho words fr fr',
+    date: 'Thu Mar 26 2020 15:16:52 GMT+1300 (New Zealand Daylight Time)',
+    author_name: 'test user',
+    topic: 'Test topic',
+  }
+
+  const date = new Date(post_data.date)
+  const formattedDate = date.toLocaleDateString()
+
+  return (
+    <div className="rounded-xl border-2 p-6 shadow-lg">
+      <div className="flex items-baseline justify-between">
+        <h1 className="text-5xl">{post_data.title}</h1>
+        <p>{formattedDate}</p>
+      </div>
+      <hr className="my-5 border-t-2 " />
+      <p className="text-lg">{post_data.entry}</p>
+      <hr className="my-5 border-t-2 " />
+      <div className="flex items-center gap-4">
+        <img
+          className="w-20 rounded-full"
+          alt="pfp"
+          src="https://i.pinimg.com/736x/9e/83/75/9e837528f01cf3f42119c5aeeed1b336.jpg"
+        ></img>
+        <p>Robert</p>
+      </div>
+    </div>
+  )
+}
+
+export default PostPage
