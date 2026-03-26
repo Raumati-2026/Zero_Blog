af40632b3f466dc7ad65fe7c5e1813b3d77b104a
Ciaran Slow <ciaranslow@hotmail.com>
2026-03-26 16:23:05 +1300

    made a change

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -28,7 +28,7 @@ function Home() {
     }
     return (
         <main className="p-6">
-            <h1 className="text-4xl font-bold">Welcome to Zero Blog</h1>
+            <h1 className="text-6xl font-bold">Welcome to Zero Blog</h1>
 
             {user && <div className="border-2 p-6 mt-6 rounded-xl shadow-md">
                 <h2 className="mb-6 text-2xl font-bold">Add Post</h2>
@@ -116,6 +116,8 @@ function Home() {
                 </div>
                 </form>
             </div>}
+
+            <h2 className="text-6xl mt-6 font-bold">Blogs</h2>
 
             <div className="shadow-xl p-6 mt-6 border-2 rounded-xl grid gap-6 grid-col-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                 <PostCard/>
