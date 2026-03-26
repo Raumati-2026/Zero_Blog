6370eea4923a0955a74030d193339d6823279c42
Ciaran Slow <ciaranslow@hotmail.com>
2026-03-26 15:53:11 +1300

    added post card to home

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,14 +1,12 @@
 import { Outlet } from 'react-router'
 import Nav from './Nav.tsx'
 import Footer from './Footer.tsx'
-import { Post } from './Post.tsx'
 
 function App() {
   return (
     <>
       <Nav />
       <Outlet />
-      <Post></Post>
       <Footer />
     </>
   )
