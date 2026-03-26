247fe227102fb52d83dfee4ef5ac8194d5814bfe
Jay Coupe <jay.huston17@gmail.com>
2026-03-26 16:06:50 +1300

    Completed the seeds file

--- server/db/seeds/posts.js
+++ server/db/seeds/posts.js
@@ -1,13 +1,33 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
-};
+export async function seed(knex) {
+  await knex('blog_entry').del()
+  await knex('blog_entry').insert([
+    {
+      id: 1,
+      title: 'My First Blog',
+      entry: 'This is my very first blog post.',
+      date: '2026-03-26',
+      author_id: 1,
+      topic: 'General',
+    },
+    {
+      id: 2,
+      title: 'Learning React',
+      entry: 'React can be pretty annoying.',
+      date: '2026-03-25',
+      author_id: 1,
+      topic: 'Coding',
+    },
+    {
+      id: 3,
+      title: 'Favourite Drink',
+      entry: 'I love coke zero.',
+      date: '2026-03-24',
+      author_id: 2,
+      topic: 'Lifestyle',
+    },
+  ])
+}
