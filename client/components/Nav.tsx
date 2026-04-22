0241548f3ec50cb1688868db4394b0a2196dfeee
phoebe <phoebe@ThinkPad-P50S.localdomain>
2026-04-22 19:07:40 +1200

    fix the auth

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,41 +1,57 @@
-import { useAuth0 } from "@auth0/auth0-react"
+import { useAuth0 } from '@auth0/auth0-react'
 
 function Nav() {
-    const auth = useAuth0()
-    const user = auth.user
-    const logout = auth.logout
-    const loginWithRedirect = auth.loginWithRedirect
+  const auth = useAuth0()
+  const user = auth.user
+  console.log(user)
 
-    const handleSignOut = () => {
-        logout()
-    }
+  const logout = auth.logout
+  const loginWithRedirect = auth.loginWithRedirect
 
-    const handleSignIn = () => {
-        loginWithRedirect()
-    }
+  const handleSignOut = () => {
+    logout()
+  }
 
-    return (
-        <nav className="flex justify-between p-6 border-b-2 items-center">
-            <div>
-                <h2 className="text-2xl font-bold">Zero Blog</h2>
+  const handleSignIn = () => {
+    loginWithRedirect()
+  }
+
+  return (
+    <nav className="flex items-center justify-between border-b-2 p-6">
+      <div>
+        <h2 className="text-2xl font-bold">Zero Blog</h2>
+      </div>
+      <div>
+        {!user ? (
+          <button
+            className="rounded-xl bg-black px-2 py-4 text-white"
+            onClick={handleSignIn}
+          >
+            Login/Sign Up
+          </button>
+        ) : (
+          <div className="flex gap-6">
+            <div className="flex items-center gap-2">
+              <img
+                src={user.picture}
+                alt="Profile"
+                className="h-14 w-14 rounded-full"
+              />
+              <p className="text-2xl font-thin">
+                {user.given_name + ' ' + user.family_name}
+              </p>
             </div>
-            <div>
-                {!user ? (
-                    <button className="py-4 px-2 bg-black text-white rounded-xl" onClick={handleSignIn}>Login/Sign Up</button>
-                ) : (
-                    <div className="flex gap-6">
-                        <div className="flex gap-2 items-center">
-                            <img src={user.picture} alt="Profile" className="rounded-full h-14 w-14" />
-                            <p className="font-thin text-2xl">{user.given_name + ' ' + user.family_name}</p>
-                        </div>
-                        <button className="py-4 px-2 bg-black text-white rounded-xl" onClick={handleSignOut}>Logout</button>
-                    </div>
-                )}
-                
-                
-            </div>
-        </nav>
-    )
+            <button
+              className="rounded-xl bg-black px-2 py-4 text-white"
+              onClick={handleSignOut}
+            >
+              Logout
+            </button>
+          </div>
+        )}
+      </div>
+    </nav>
+  )
 }
 
 export default Nav
